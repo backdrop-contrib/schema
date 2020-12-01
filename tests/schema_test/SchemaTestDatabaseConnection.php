ca9e5f2d92a9f87775fe22e52d03775068438101
indigoxela <user@olga.indigofloat.at>
2020-12-01 16:43:45 +0100

    Get functional tests running with Backdrop

--- tests/schema_test/SchemaTestDatabaseConnection.php
+++ tests/schema_test/SchemaTestDatabaseConnection.php
@@ -6,7 +6,7 @@
  */
 
 class SchemaTestDatabaseConnection extends DatabaseConnection {
-  function __construct(array $connection_options = array()) {
+  public function __construct(array $connection_options = array()) {
     $this->connectionOptions = $connection_options;
     $this->setPrefix(!empty($this->connectionOptions['prefix']) ? $this->connectionOptions['prefix'] : '');
   }
@@ -15,7 +15,7 @@ class SchemaTestDatabaseConnection extends DatabaseConnection {
     return NULL;
   }
 
-  function queryTemporary($query, array $args = array(), array $options = array()) {
+  public function queryTemporary($query, array $args = array(), array $options = array()) {
     return NULL;
   }
 
@@ -34,4 +34,9 @@ class SchemaTestDatabaseConnection extends DatabaseConnection {
   public function nextId($existing_id = 0) {
     return NULL;
   }
+
+  public function createDatabase($database) {
+    return NULL;
+  }
+
 }
